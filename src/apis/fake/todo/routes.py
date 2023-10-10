cacfddd09373f7cdd100f603e2fa16f2b91e0fd5
Deimian Vásquez <deimianvasquez@gmail.com>
2023-10-10 02:05:00 -0400

    id is added, the put methid is update to validated that it has label and task only

--- src/apis/fake/todo/routes.py
+++ src/apis/fake/todo/routes.py
@@ -5,7 +5,7 @@ from flask import Blueprint, jsonify, request, render_template
 
 todo = Blueprint('todo', __name__)
 
-todo_file_path = os.path.join(os.path.dirname(__file__), 'todos.json')
+todo_path = os.path.join(os.path.dirname(__file__), 'todos.json')
 
 
 @todo.route('/', methods=['GET'])
@@ -17,15 +17,14 @@ def handle_index():
 @todo.route('/user', methods=['GET'])
 def get_all_users():
     if request.method == 'GET':
-        todos = Todo.create_json_file(todo_file_path)
-        users = []
+        todos = Todo.create_json_file(todo_path)
+
         if todos is None:
             return jsonify({"msg": "Internal server error"}), 500
         
         if todos is True:
-            todos = Todo.get_all_users(todo_file_path)
+            todos = Todo.get_all_users(todo_path)
             return jsonify(todos), 200
-    
 
 
 @todo.route('/user/<string:username>', methods=['GET'])
@@ -34,7 +33,7 @@ def get_all_todo(username=None):
         if username is None:
             return jsonify({"msg": "bad request"}), 400
         
-        todos = Todo.get_all_todo_by_user(todo_file_path, username)
+        todos = Todo.get_all_todo_by_user(todo_path, username)
 
         if todos is None:
             return jsonify({"msg": f"The user {username} doesn't exists"}), 404
@@ -42,47 +41,57 @@ def get_all_todo(username=None):
         if todos is not None:
             return jsonify(todos), 200
         
-    
 
 @todo.route('/user/<string:username>', methods=['PUT'])
 def update_task(username=None):
     if request.method == 'PUT':
+        Todo.create_json_file(todo_path)
         if username is None:
             return jsonify({"msg": "You must include a username in the URL of the request"}), 400
         
-        todos = Todo.get_all_todo_by_user(todo_file_path, username)
+        todos = Todo.get_all_todo_by_user(todo_path, username)
         if todos is None:
             return jsonify({"msg": f"The user {username} doesn't exists"}), 404
-        
-
+            
         data = request.json
+        if type(data) != list:
+            return jsonify({"msg": "You must send an array in the body of the request"}), 400
+
         if len(data) >= 1:
-            update_todo = Todo.update_all_todos_by_user(todo_file_path, username, data)
-
-            if update_todo is True:
-                return jsonify({"msg": f"{len(data)} tasks were updated successfully"}), 200
-            else:
-                return jsonify({"msg": "Internal server error"}), 500
+            for task in data:
+                if type(task) != dict:
+                    return jsonify({"msg": "You must send an array of objects in the body of the request"}), 400
+                if set(("label", "done")).issuperset(task):
+                    if type(task['label']) != str:
+                        return jsonify({"msg": "The label must be a string"}), 400
+                    if type(task['done']) != bool:
+                        return jsonify({"msg": "The done must be a boolean"}), 400
+                else:
+                    return jsonify({"msg": "You must send an array of objects with the following properties: label and done"}), 400   
         else:
-            return jsonify({"msg": "You must send at least one task"}), 400
-
-
-
-
+            return jsonify({"msg": "You must send at least one task"}), 400 
+        
+        update_todo = Todo.update_all_todos_by_user(todo_path, username, data)
+        if update_todo is True:
+            return jsonify({"msg": f"{len(data)} tasks were updated successfully"}), 200
+        else:
+            return jsonify({"msg": "Internal server error"}), 500
+    
+            
 @todo.route('/user/<string:username>', methods=['POST'])
 def create_user(username=None):
-    Todo.create_json_file(todo_file_path)
+    Todo.create_json_file(todo_path)
     try:
         data = request.json
     except Exception as error:
         return jsonify({"message": "You must add an empty array in the body of the request"}), 400
 
     if type(data) == list and len(data) == 0:
-        users = Todo.get_all_users(todo_file_path)
+        users = Todo.get_all_users(todo_path)
         if username in users:
             return jsonify({"msg": "The user exist"}), 400
         else:
-            user = Todo.create_user(todo_file_path, username)
+            user = Todo.create_user(todo_path, username)
             if user is True:
                 return jsonify({"msg": f"The user {username} has been created successfully"}), 201
             else:
@@ -97,11 +106,11 @@ def delete_task(username=None):
     if username is None:
         return jsonify({"msg": "You must include a username in the URL of the request"}), 400
 
-    users = Todo.get_all_users(todo_file_path)
+    users = Todo.get_all_users(todo_path)
     if username not in users:
         return jsonify({"msg": f"The user {username} doesn't exist"}), 404
     else:
-        user_delete = Todo.delete_user(todo_file_path, username)
+        user_delete = Todo.delete_user(todo_path, username)
         if user_delete is None:
             return jsonify({"msg": "Internal server error"}), 500
         return jsonify({"msg": f"The user {username} has been deleted successfully"}), 201
