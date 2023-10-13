3da6114ec4807542422d1e64aff721212af60e32
Deimian Vásquez <deimianvasquez@gmail.com>
2023-10-13 00:45:50 -0400

    Add the markdown library for documentation

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, url_for, render_template
 from flask_cors import CORS
 from flask_bootstrap import Bootstrap
+import markdown
+
 
 from apis.fake.todo.routes import todo
 from apis.fake.contacts.routes import contact
@@ -21,7 +23,9 @@ app.register_blueprint(kill_the_bug, url_prefix='/apis/kill-the-bug')
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    readme_html = open('README.md', 'r').read()
+    readme_html = markdown.markdown(readme_html)
+    return render_template('home.html', content=readme_html)
 
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 5000))
